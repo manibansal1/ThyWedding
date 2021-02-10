ea161847041ae91c9cbffc236219101e1ed45296
manibansal1 <47379449+manibansal1@users.noreply.github.com>
2021-02-11 00:44:37 +1000

    IDD-Landing page-1 Minor changes done..Working on imgs path

---
+++ js/indes.js
@@ -0,0 +1,51 @@
+
+        function budgetDescription() {
+          var x = document.getElementById("budget-description");
+          if (x.style.display === "none") {
+            x.style.display = "block";
+          } else {
+            x.style.display = "none";
+          }
+        }
+        function eventDescription() {
+          var x = document.getElementById("Event-Plannerbtn");
+          if (x.style.display === "none") {
+            x.style.display = "block";
+          } else {
+            x.style.display = "none";
+          }
+        }
+        function invitationDescription() {
+          var x = document.getElementById("invitationbtn");
+          if (x.style.display === "none") {
+            x.style.display = "block";
+          } else {
+            x.style.display = "none";
+          }
+        }
+        function guestDescription() {
+          var x = document.getElementById("guestbtn");
+          if (x.style.display === "none") {
+            x.style.display = "block";
+          } else {
+            x.style.display = "none";
+          }
+        }
+        function bookDescription() {
+          var x = document.getElementById("bookbtn");
+          if (x.style.display === "none") {
+            x.style.display = "block";
+          } else {
+            x.style.display = "none";
+          }
+        }
+        function installmentDescription() {
+          var x = document.getElementById("installmentbtn");
+          if (x.style.display === "block") {
+            x.style.display = "none";
+          } else {
+            x.style.display = "block";
+          }
+        }
+        
+   
